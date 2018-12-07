db03b11d8a5aafb5e8dc6c37dc371213ed19777c
skyfeature <skyfeature@gmail.com>
2018-12-08 01:21:16 +0530

    exceptions

---
+++ Basic Exception.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+void mightGoWrong() {
+	bool error0 = false;
+	bool error1 = false;
+	bool error2 = true;
+
+	if(error0) {
+		throw 8;
+	}
+	if(error1) {
+		throw "Something went wrong.";
+	}
+
+	if(error2) {
+		throw string("Something else went wrong.");
+	}
+
+}
+
+int main() {
+	try {
+		mightGoWrong();
+	}
+	catch(int e) {
+		cout << "Error code: " << e << "\n";
+	}
+	catch(char const *e) {
+		cout << "Error message: " << e << "\n";
+	}
+	catch(string &e) {
+		cout << "Error message: " << e << "\n";
+	}
+
+	cout << "Still running"
+}
